480c7ff032a98a930b3130d73748766c15621fed
Omar Kamel <tekexplorerm@gmail.com>
2024-12-14 03:45:45 -0500

    make it possible to use other output types

--- lib/files.dart
+++ lib/files.dart
@@ -6,18 +6,22 @@ extension FileExists on File {
   Future<void> ensureExists() async {
     if (!await exists()) await create();
   }
+
+  void ensureExistsSync() {
+    if (!existsSync()) createSync();
+  }
 }
 
 final _dir = posix.dirname(Platform.script.path);
-final _relativeDir = posix.relative(
+final relativeDir = posix.relative(
   _dir,
   // move up two steps for useful stuff
   from: posix.dirname(posix.dirname(_dir)),
 );
 
-final input = File(posix.join(_relativeDir, 'input.txt'));
+final input = File(posix.join(relativeDir, 'input.txt'));
 
 final inputContents = input.readAsStringSync();
 
-final output1 = File(posix.join(_relativeDir, 'output1.txt'));
-final output2 = File(posix.join(_relativeDir, 'output2.txt'));
+final output1 = File(posix.join(relativeDir, 'output1.txt'));
+final output2 = File(posix.join(relativeDir, 'output2.txt'));
