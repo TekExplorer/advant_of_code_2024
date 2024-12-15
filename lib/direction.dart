c08ec6c1f87b13416a51da34fb5c6dfdbc8c762d
Omar Kamel <tekexplorerm@gmail.com>
2024-12-15 02:09:01 -0500

    Add Direction utilities

--- lib/direction.dart
+++ lib/direction.dart
@@ -6,11 +6,11 @@ enum Direction {
   left,
   right;
 
-  XY modify(XY pos) => switch (this) {
-        Direction.up => (x: pos.x, y: pos.y - 1),
-        Direction.down => (x: pos.x, y: pos.y + 1),
-        Direction.left => (x: pos.x - 1, y: pos.y),
-        Direction.right => (x: pos.x + 1, y: pos.y),
+  XY modify(XY pos, [int times = 1]) => switch (this) {
+        Direction.up => (x: pos.x, y: pos.y - times),
+        Direction.down => (x: pos.x, y: pos.y + times),
+        Direction.left => (x: pos.x - times, y: pos.y),
+        Direction.right => (x: pos.x + times, y: pos.y),
       };
 
   Direction get rotated90 => switch (this) {
@@ -27,6 +27,31 @@ enum Direction {
         Direction.left => right,
         Direction.right => left,
       };
+
+  bool get isHorizontal => this == Direction.left || this == Direction.right;
+  bool get isVertical => this == Direction.up || this == Direction.down;
+}
+
+extension XYDirection on XY {
+  XY shift(Direction direction, [int times = 1]) =>
+      direction.modify(this, times);
+  XY get up => shift(Direction.up);
+  XY get down => shift(Direction.down);
+  XY get left => shift(Direction.left);
+  XY get right => shift(Direction.right);
+
+  XY shiftDiagonal(Diagonal diagonal, [int times = 1]) =>
+      diagonal.modify(this, times);
+
+  XY get upLeft => up.left;
+  XY get upRight => up.right;
+  XY get downLeft => down.left;
+  XY get downRight => down.right;
+
+  // XY get upLeft => shiftDiagonal(Diagonal.upLeft);
+  // XY get upRight => shiftDiagonal(Diagonal.upRight);
+  // XY get downLeft => shiftDiagonal(Diagonal.downLeft);
+  // XY get downRight => shiftDiagonal(Diagonal.downRight);
 }
 
 enum Diagonal {
@@ -42,11 +67,11 @@ enum Diagonal {
         Diagonal.downRight => {Direction.down, Direction.right},
       };
 
-  XY modify(XY pos) => switch (this) {
-        Diagonal.upLeft => (x: pos.x - 1, y: pos.y - 1),
-        Diagonal.upRight => (x: pos.x + 1, y: pos.y - 1),
-        Diagonal.downLeft => (x: pos.x - 1, y: pos.y + 1),
-        Diagonal.downRight => (x: pos.x + 1, y: pos.y + 1),
+  XY modify(XY pos, [int times = 1]) => switch (this) {
+        Diagonal.upLeft => (x: pos.x - times, y: pos.y - times),
+        Diagonal.upRight => (x: pos.x + times, y: pos.y - times),
+        Diagonal.downLeft => (x: pos.x - times, y: pos.y + times),
+        Diagonal.downRight => (x: pos.x + times, y: pos.y + times),
       };
 
   Diagonal get rotated90 => switch (this) {
